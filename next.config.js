c2b802d54e9d5190ec0f4a1cb7abfe573f410599
Agustín Pereira <a.pereira.clavijo.25@gmail.com>
2023-03-02 10:20:44 -0300

    cambio nextconfig

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  staticPageGenerationTimeout: 1000, //Agregado para que Render solucione el error
 }
 
 module.exports = nextConfig
